f7805e5791f0828efae799e5da9cac346ffd3b7a
soon <makt00byoung@gmail.com>
2024-05-02 16:27:26 +0900

    전반적인 css 수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // aos 적용
+// aos 적용
   AOS.init();
-  // 탑버튼
+// 탑버튼
   const topButton = document.getElementById("top-button");
   window.addEventListener("scroll", function () {
     if (window.scrollY > 0) {
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
       topButton.style.display = "none"; // 숨기기
     }
   });
-  //검색창
+//검색창
   const searchEl = document.querySelector(".search");
   const searchInputEl = searchEl.querySelector("input");
   searchEl.addEventListener("click", function () {
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     searchEl.classList.remove("focused");
     searchInputEl.setAttribute("placeholder", "");
   });
-  // 햄버거 버튼, 오버레이
+// 햄버거 버튼, 오버레이
   const mbt = document.querySelector(".mbt");
   const navMb = document.querySelector(".nav-mb");
 
@@ -41,7 +41,30 @@ document.addEventListener("DOMContentLoaded", function () {
       navMb.classList.add("active");
     }
   });
-  // 프로그램
+// 카테고리 스크롤
+  $(document).ready(function () {
+    var menu_start02 = "0";
+    var $wrap = $("#mobile_nav02").parent(); //스크롤을 넣고 싶을 경우 삽입
+    $("#mobile_nav02").sly({
+      horizontal: 1,
+      itemNav: "centered",
+      smart: 1,
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      startAt: menu_start02,
+      scrollBar: $wrap.find(".scrollbar"), //스크롤을 넣고 싶을 경우 삽입
+      scrollBy: 1, //스크롤을 넣고 싶을 경우 삽입
+      speed: 300,
+      elasticBounds: 1,
+      dragHandle: 1,
+      dynamicHandle: 1,
+    });
+    $(window).resize(function (e) {
+      $("#mobile_nav02").sly("reload");
+    });
+  });
+// 프로그램
   var swiper = new Swiper(".sw-s_program", {
     slidesPerView: 1.5,
     centeredSlides: true,
@@ -67,13 +90,13 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       400: {
         slidesPerView: 1.5,
-      }
+      },
     },
     autoplay: {
-      delay: 2000, // 슬라이드 간의 시간 간격 (밀리초)
+      delay: 2000,
     },
   });
-  // <!-- 이달의 할인 배너 및 리뷰 적립 -->
+// 이달의 할인 배너 및 리뷰 적립
   var swiper = new Swiper(".sw-s_banner2", {
     spaceBetween: 30,
     pagination: {
@@ -84,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function () {
       delay: 3000,
     },
   });
-  // 처음이라면
+// 처음이라면
   var swiper = new Swiper(".sw-s_foryou", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -110,14 +133,14 @@ $(document).ready(function () {
     let submenu = $(this).next(".hide");
     submenu.slideToggle(); // 부드럽게 슬라이드 업 또는 다운
 
-    // 화살표 회전 토글
+ // 화살표 회전 토글
     $(this).find(".material-symbols-outlined").toggleClass("active");
   });
-  // 하트 채움
-  $(document).ready(function() {
-    $('.heart').click(function() {
-      event.preventDefault()
-      $(this).toggleClass('far fas');
+// 하트 채움
+  $(document).ready(function () {
+    $(".heart").click(function () {
+      event.preventDefault();
+      $(this).toggleClass("far fas");
     });
   });
 });
